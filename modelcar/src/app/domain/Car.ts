ee1259bdaf2c8fb4e1415a6371366dde34ed8360
peterdp <peter.de.permentier@gmail.com>
2017-10-20 22:29:30 +0200

    added fields to Car

--- modelcar/src/app/domain/Car.ts
+++ modelcar/src/app/domain/Car.ts
@@ -1,9 +1,11 @@
 import {Brand} from './Brand';
 import {Color} from './Color';
+import {Model} from './Model';
 
 export class Car {
   itemRef: string;
   brand: Brand;
+  model: Model;
   color: Color;
   limitedEdition: number;
   coupe: boolean;
